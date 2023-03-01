153d898fbe9da53a4c8b6f17c0e30010ed65ee5a
Muslim Uddin Arju <mdarju240@gmail.com>
2023-03-01 13:07:18 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+import Sidebar from './components/layouts/Sidebar';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Questions from './pages/Questions/Questions';
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {isLogged ?
+          <div>
+            <Header setIsLogged={setIsLogged} />
+            <Sidebar />
+            <main id="main" class="main">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/questions" element={<Questions />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+          :
+          <Routes>
+            <Route path="/" element={<Login setIsLogged={setIsLogged} />} />
+          </Routes>
+        }
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Questions/Questions.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Questions() {
+    return (
+        <div>
+            <div class="pagetitle">
+                <h1>Questions</h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item active">Question</li>
+                    </ol>
+                </nav>
+            </div>
+            <section>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="card p-3">
+                                <h3>Manage Questions</h3>
+
+                                table will show here
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
